1424561df7d06555b78be2c612a1fe3dad812aa8
Mark Ryan <markrryan99@gmail.com>
2024-02-21 18:27:56 -0800

    he be listening

--- bot.js
+++ bot.js
@@ -21,6 +21,22 @@ const opossumFacts = [
     "Opossums are avid gamers, organizing video game tournaments in their burrows, complete with tiny controllers and multiplayer competitions."
 ];
 
+let opossumTriggers = [
+  "blossom", "awesome", "bottom", "column", "blossomed", "are some", "on some", "quantum", "was some", "solemn", "got some", 
+  "blossoms", "not some", "saddam", "scrotum", "cause some", "sodom", "bonum", "colum", "dalam", "menachem", "popham", 
+  "flotsam", "menahem", "higginbotham", "gotham", "bochum", "sholom", "sham", "slalom", "vol ume", "col umn", "osten", 
+  "kosten", "crossan", "moslem", "saw some", "draw some", "was come", "across some", "was numb", "caution", "fossil", 
+  "gossip", "apostle", "colossal", "docile", "casas", "boss of", "colossus", "nasir", "rosin", "manassas", "proboscis", 
+  "prices", "hypoglossal", "nasr", "kossuth", "wasn", "laplacian", "jostle", "philosophe", "basile", "yasser", "posset", 
+  "problem", "to some", "the sum", "not come", "columns", "be some", "although some", "me some", "you some", "through some", 
+  "harlem", "see some", "to sum", "fossa", "casa", "us some", "use some", "why some", "bottoms", "show some", "asa", 
+  "postpartum", "this sum", "how some", "discuss some", "condom", "want some", "buy some", "markham", "caused some", 
+  "large sum", "cause him", "unless some", "gruesome", "know some", "produce some", "so some", "involve some", "now some", 
+  "play some", "introduce some", "bottomed", "away some", "allow some", "plus some", "try some", "review some", "throw some", 
+  "barnum", "was some", "way some", "thus some", "are summed", "possess some", "pay some", "say some", "this some", "knew some", 
+  "stardom", "solve some", "enjoy some", "adopt some", "aspartame"
+];
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
@@ -30,13 +46,36 @@ client.on('messageCreate', (message) => {
   if (message.author.bot) return;
 
   // Check if the message contains the trigger word
-  if (message.content.includes('random')) {
+  if (message.content.includes('!opossumfact')) {
     // Get a random item from the array
     const randomOpossumFact = opossumFacts[Math.floor(Math.random() * opossumFacts.length)];
 
     // Send the random item as a reply
     message.reply(`**${randomOpossumFact}**`);
+    // message.channel.send(`**${randomOpossumFact}**`);
   }
 });
+
+client.on('messageCreate', (message) => {
+  // Ignore messages from the bot itself
+  if (message.author.bot) return;
+
+  // Check if the message contains any trigger words
+  const containsOpossumTrigger = opossumTriggers.some(word => message.content.toLowerCase().includes(word.toLowerCase()));
+
+  // If a trigger word is found, respond with a message
+  if (containsOpossumTrigger) {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(randomNumber)
+
+    // There's a 1 in 100 chance of saying something else
+    if (randomNumber === 1) {
+        message.channel.send('Borgbot --overdrive');
+    } else {
+        message.channel.send('Re re reer');
+    }
+}
+});
+
 
 client.login(process.env.TOKEN);
